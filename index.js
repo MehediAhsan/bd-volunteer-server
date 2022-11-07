a247698be955167cd6c614460be73d94d440c028
Md Mehedi Ahsan <ahsanmehedi041@gmail.com>
2022-11-07 14:38:12 +0600

    status updated

--- index.js
+++ index.js
@@ -54,6 +54,19 @@ async function run(){
             res.send(result)
         })
 
+        app.patch('/donates/:id', async (req, res) => {
+            const id = req.params.id;
+            const status = req.body.status
+            const query = { _id: ObjectId(id) }
+            const updatedDoc = {
+                $set:{
+                    status: status
+                }
+            }
+            const result = await donateCollection.updateOne(query, updatedDoc);
+            res.send(result);
+        })
+
         app.delete('/donates/:id' , async(req , res)=>{
             const id = req.params.id;
             const query = {_id: ObjectId(id)}
